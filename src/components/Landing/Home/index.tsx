9edaf041062b7dbd66c0623df8c2b92b47a3b945
juliuz94 <julian.lozano.fr@hotmail.com>
2023-05-04 10:05:56 -0500

    Initial config for landing page

---
+++ src/components/Landing/Home/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+const Home:FC = () => {
+  return (
+    <div>LANDING</div>
+  )
+}
+
+export default Home
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import { useRouter } from 'next/router'
 import { Button } from 'antd'
+import Home from '@/components/Landing/Home'
 
 export default function Index() {
   const router = useRouter()
   return (
-    <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-      <Button onClick={() => router.push('/login')}>
-        Log In
-      </Button>
-    </div>
+    <Home />
   )
 }
